25c437a4d43ad7eb833da3c1b944197455186f7f
allisonvinicius <allisonvinicius63@gmail.com>
2022-11-05 00:16:55 -0400

    estruture initial spring-boot

---
+++ vendas/src/main/java/com/io/github/allison/vendas/VendasApplication.java
@@ -0,0 +1,17 @@
+package com.io.github.allison.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class VendasApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasApplication.class, args);
+		
+	}
+
+}
---
+++ vendas/src/test/java/com/io/github/allison/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.allison.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
