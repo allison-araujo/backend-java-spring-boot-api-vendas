e15d9e8fd1f64b85fc3a8bc7ac6ac3667c34d407
allisonvinicius <allisonvinicius63@gmail.com>
2022-11-06 22:40:26 -0400

    refactory: update files

--- vendas/src/main/java/com/io/github/allison/VendasApplication.java
+++ vendas/src/main/java/com/io/github/allison/VendasApplication.java
@@ -1,9 +1,7 @@
 package com.io.github.allison;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,19 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 public class VendasApplication {
-
-
-	@Value("${application.name}")
-
-	private String applicationName;
-
-	@GetMapping("/hello")
-	public String helloword(){
-		return applicationName;
-
-	}
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 		
