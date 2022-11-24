078cf17e3864ae24ac0a28ba9b364b20e85be280
allisonvinicius <allisonvinicius63@gmail.com>
2022-11-23 23:25:43 -0400

    refactory:  include anootations jpa in class

--- vendas/src/main/java/com/io/github/allison/domain/entity/Produto.java
+++ vendas/src/main/java/com/io/github/allison/domain/entity/Produto.java
@@ -2,10 +2,29 @@ package com.io.github.allison.domain.entity;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "produto")
 public class Produto {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "descricao", length = 100)
     private String descricao;
+
+    @Column(name = "preco")
     private BigDecimal preco;
 
 
