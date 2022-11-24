b100f38fed10a727ebb44fe73d0ed75a89402061
allisonvinicius <allisonvinicius63@gmail.com>
2022-11-23 23:26:35 -0400

    refactory:  anotations query search in interface

--- vendas/src/main/java/com/io/github/allison/domain/repositorio/Clientes.java
+++ vendas/src/main/java/com/io/github/allison/domain/repositorio/Clientes.java
@@ -3,6 +3,7 @@ package com.io.github.allison.domain.repositorio;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,10 +13,13 @@ public interface Clientes extends JpaRepository<Cliente, Integer> {
 
 
     
-    @Query(value ="select c from Cliente c where c.nome like :nome ")
+    @Query(value ="select * from cliente c where c.nome like '%:nome' ", nativeQuery = true)
     List<Cliente> findByNomeLike(@Param("nome") String nome);
 
-
+    
+    @Query(" delete from Cliente c where c.nome = :nome")
+    @Modifying
+    void deleteByNome(String nome);
 
     boolean existsByNome(String string);
 
