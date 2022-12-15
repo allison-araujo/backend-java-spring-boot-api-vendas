79814addd286f22de993b1b48f469572aeb800c7
allisonvinicius <allisonvinicius63@gmail.com>
2022-12-14 23:55:13 -0400

    hotfix: update controllerCliente in methods get

--- vendas/src/main/java/com/io/github/allison/controller/ClienteController.java
+++ vendas/src/main/java/com/io/github/allison/controller/ClienteController.java
@@ -1,23 +1,35 @@
 package com.io.github.allison.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.io.github.allison.domain.entity.Cliente;
+import com.io.github.allison.domain.repository.Clientes;
+
 @Controller
-@RequestMapping("/api/clientes")
 public class ClienteController {
     
-    @RequestMapping(
-        value = "/hello/{nome}", 
-        method = RequestMethod.POST,
-        consumes = { "application/json", "application/xml"}
-    )
-        @ResponseBody
-        public String helloCliente( @PathVariable("nome") String nomeCliente){
-        return String.format("Hello %s", nomeCliente);
+    private Clientes clientes;
+
+    
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id){
+      Optional<Cliente> cliente = clientes.findById(id);
+      if(cliente.isPresent()){
+        return ResponseEntity.ok(cliente.get());
+      }
+
     }
 
 
