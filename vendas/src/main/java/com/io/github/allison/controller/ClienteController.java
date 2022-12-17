6a0b6c992d16fa0a6b76a7029e9010837ec7df95
allisonvinicius <allisonvinicius63@gmail.com>
2022-12-16 21:36:50 -0400

    feat: feature save cliente in class controllerCliente

--- vendas/src/main/java/com/io/github/allison/controller/ClienteController.java
+++ vendas/src/main/java/com/io/github/allison/controller/ClienteController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.io.github.allison.domain.entity.Cliente;
@@ -32,6 +34,18 @@ public class ClienteController {
       return  ResponseEntity.notFound().build();
 
     }
+
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save(@RequestBody Cliente cliente){
+     Cliente clienteSave =  clientes.save(cliente);
+
+      return ResponseEntity.ok(clienteSave);
+
+    }
+
+
 
 
 }
