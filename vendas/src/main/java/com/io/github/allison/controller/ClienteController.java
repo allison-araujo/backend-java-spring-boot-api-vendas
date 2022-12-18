e1200da0a7899ca10b5a8984eb653380086b2f89
allisonvinicius <allisonvinicius63@gmail.com>
2022-12-17 23:47:53 -0400

    feat: method delete clientes implements

--- vendas/src/main/java/com/io/github/allison/controller/ClienteController.java
+++ vendas/src/main/java/com/io/github/allison/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,17 @@ public class ClienteController {
 
     }
 
+    @DeleteMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity delete( @PathVariable Integer id){
+      Optional<Cliente> cliente = clientes.findById((id);
+
+      if(cliente.isPresent()){
+       clientes.delete(cliente.get());
+       return ResponseEntity.noContent().build();
+      }
+
+    }
 
 
 
--- vendas/src/main/java/com/io/github/allison/domain/entity/Cliente.java
+++ vendas/src/main/java/com/io/github/allison/domain/entity/Cliente.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "cliente")
 
@@ -23,7 +25,8 @@ public class Cliente {
         
     @Column(name = "nome", length = 100)
     private String nome;
-
+    
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
 
