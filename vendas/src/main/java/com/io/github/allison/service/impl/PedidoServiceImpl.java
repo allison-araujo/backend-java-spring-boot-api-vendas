b689e245cc14f804ba1385e381424708038be903
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-01 23:28:41 -0400

    feat: implements file service

--- vendas/src/main/java/com/io/github/allison/service/impl/PedidoServiceImpl.java
+++ vendas/src/main/java/com/io/github/allison/service/impl/PedidoServiceImpl.java
@@ -1,5 +1,18 @@
 package com.io.github.allison.service.impl;
 
-public class PedidoServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.io.github.allison.domain.repository.Pedidos;
+import com.io.github.allison.service.PedidoService;
+
+@Service
+public class PedidoServiceImpl implements PedidoService {
+    private Pedidos repository;
+
+    public PedidoServiceImpl(Pedidos repository) {
+        this.repository = repository;
+    }
+
+
     
 }
