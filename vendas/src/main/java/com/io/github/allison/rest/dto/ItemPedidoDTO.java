be7ae59dcb79a9383d98f46e84b8460909bf1c79
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-03 00:05:41 -0400

    feat: created class implments DTO

---
+++ vendas/src/main/java/com/io/github/allison/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,15 @@
+package com.io.github.allison.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemPedidoDTO {
+    private Integer produto;
+    private Integer quantidade;
+
+    
+}
