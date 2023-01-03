5c242171be2e2b56467c69564d0d6640c23077da
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-03 14:21:18 -0400

    feat: methods save request dto

--- vendas/src/main/java/com/io/github/allison/service/PedidoService.java
+++ vendas/src/main/java/com/io/github/allison/service/PedidoService.java
@@ -1,5 +1,10 @@
 package com.io.github.allison.service;
 
+import com.io.github.allison.domain.entity.Pedido;
+import com.io.github.allison.rest.dto.PedidoDTO;
+
 public interface PedidoService {
+
+    Pedido salvar(PedidoDTO dto );
     
 }
