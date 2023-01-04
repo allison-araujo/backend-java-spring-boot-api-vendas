35917dc867984e8168d83d66139ef7a3752aefef
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-04 00:11:13 -0400

    refactory: include methods save pedido

--- vendas/src/main/java/com/io/github/allison/service/impl/PedidoServiceImpl.java
+++ vendas/src/main/java/com/io/github/allison/service/impl/PedidoServiceImpl.java
@@ -1,18 +1,62 @@
 package com.io.github.allison.service.impl;
 
-import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.io.github.allison.domain.entity.Cliente;
+import com.io.github.allison.domain.entity.ItemPedido;
+import com.io.github.allison.domain.entity.Pedido;
+import com.io.github.allison.domain.repository.Clientes;
+import com.io.github.allison.domain.repository.ItemsPedido;
 import com.io.github.allison.domain.repository.Pedidos;
+import com.io.github.allison.domain.repository.Produtos;
+import com.io.github.allison.exception.RuleBusinessException;
+import com.io.github.allison.rest.dto.PedidoDTO;
 import com.io.github.allison.service.PedidoService;
 
-@Service
-public class PedidoServiceImpl implements PedidoService {
-    private Pedidos repository;
+import lombok.RequiredArgsConstructor;
 
-    public PedidoServiceImpl(Pedidos repository) {
-        this.repository = repository;
-    }
+@Service
+@RequiredArgsConstructor
+public class PedidoServiceImpl implements PedidoService {
+
+    private final Pedidos repository;
+    private final Clientes clientesRepository;
+    private final Produtos produtosRepository;
+    private final ItemsPedido itemsPedidoRepository;
+
 
 
     
+     @Override 
+     @Transactional
+     public Pedido salvar(PedidoDTO dto){
+        Integer idCliente = dto.getCliente(); 
+        Cliente cliente = clientesRepository
+            .findById(idCliente)
+            .orElseThrow(() -> new RuleBusinessException("Codigo de Cliente invalido!!"));
+
+
+        Pedido pedido = new Pedido();
+        pedido.setTotal(dto.getTotal());
+        pedido.setDataPedido(LocalDate.now());
+        pedido.setCliente(cliente);
+
+        List<ItemPedido> itemsPedido = converterItems(pedido, dto.getItems());
+        repository.save(pedido);
+        itemsPedidoRepository.saveAll(itemsPedido);
+        pedido.setItens(itemsPedido);
+
+        return pedido;
+    }
+    
+
+
+ 
+
+
+
 }
