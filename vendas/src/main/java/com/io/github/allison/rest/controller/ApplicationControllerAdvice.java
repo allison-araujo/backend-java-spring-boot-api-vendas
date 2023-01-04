aa3c4c8893a6156f6911dd44d7c1d44daa0894bc
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-04 00:14:32 -0400

    feat: include class treatment erros in application

---
+++ vendas/src/main/java/com/io/github/allison/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.io.github.allison.rest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.io.github.allison.exception.RuleBusinessException;
+import com.io.github.allison.rest.ApiErros;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RuleBusinessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErros handleRuleBusinnesException(RuleBusinessException ex){
+
+        String mensagemErro = ex.getMessage();
+        return new ApiErros(mensagemErro);
+
+
+    }
+    
+}
