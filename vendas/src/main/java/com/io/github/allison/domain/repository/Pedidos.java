3068c908b8c21d5eade18eeaa95293736617e1e9
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-03 23:55:19 -0400

    feat: include pedidoDTO class

--- vendas/src/main/java/com/io/github/allison/domain/repository/Pedidos.java
+++ vendas/src/main/java/com/io/github/allison/domain/repository/Pedidos.java
@@ -1,6 +1,7 @@
 package com.io.github.allison.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,6 @@ import com.io.github.allison.domain.entity.Pedido;
 public interface Pedidos  extends JpaRepository<Pedido, Integer>{
     
     List<Pedido>  findByCliente(Cliente cliente);
+
+    Optional<Pedido> findByIdFetchItens(Integer id);
 }
--- vendas/src/main/java/com/io/github/allison/rest/dto/DetailsItemPedidoDTO.java
+++ vendas/src/main/java/com/io/github/allison/rest/dto/DetailsItemPedidoDTO.java
@@ -1,18 +1,20 @@
 package com.io.github.allison.rest.dto;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PedidoDTO {
-    private Integer cliente;
-    private BigDecimal total;
-    private List<ItemPedidoDTO> items;
+@Builder
+
+public class DetailsItemPedidoDTO {
+    private String descricaoProduto;
+    private BigDecimal precoUnitario;
+    private Integer quantidade;
 
 }
