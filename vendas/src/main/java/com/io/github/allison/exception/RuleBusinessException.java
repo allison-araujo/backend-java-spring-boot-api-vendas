98c291c519562b7c0afce6691a57c78b85bb2d07
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-03 23:53:45 -0400

    feat: cretae treatment error customize

---
+++ vendas/src/main/java/com/io/github/allison/exception/RuleBusinessException.java
@@ -0,0 +1,9 @@
+package com.io.github.allison.exception;
+
+public class RuleBusinessException extends RuntimeException{
+    
+    
+    public RuleBusinessException(String message){
+        super(message);
+    }
+}
---
+++ vendas/src/main/java/com/io/github/allison/rest/ApiErros.java
@@ -0,0 +1,22 @@
+package com.io.github.allison.rest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.Getter;
+
+
+
+public class ApiErros {
+
+    @Getter
+    private List<String> errors;
+    
+    public ApiErros(String mensagemError){
+        this.errors = Arrays.asList(mensagemError);
+        
+    }
+
+
+
+}
