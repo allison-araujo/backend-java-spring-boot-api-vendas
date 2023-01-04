5ec60f893005f0f8143b612540e4a572b292f701
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-03 22:14:21 -0400

    feat: include methods save pedido

--- vendas/src/main/java/com/io/github/allison/rest/controller/PedidoController.java
+++ vendas/src/main/java/com/io/github/allison/rest/controller/PedidoController.java
@@ -1,8 +1,14 @@
 package com.io.github.allison.rest.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.io.github.allison.domain.entity.Pedido;
+import com.io.github.allison.rest.dto.PedidoDTO;
 import com.io.github.allison.service.PedidoService;
 
 @RestController
@@ -16,7 +22,12 @@ public class PedidoController {
     }
 
 
-    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Integer save(@RequestBody PedidoDTO dto){
+        Pedido pedido = service.salvar(dto);
+        return pedido.getId();
+    }
 
     
 }
