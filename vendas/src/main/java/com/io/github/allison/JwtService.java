a761d5f6f906ffaae1e8ad3267dfa9169f93f6ca
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-22 21:11:47 -0400

    feat: methods generate token end verified validation

--- vendas/src/main/java/com/io/github/allison/JwtService.java
+++ vendas/src/main/java/com/io/github/allison/JwtService.java
@@ -1,7 +1,21 @@
 package com.io.github.allison;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
+
+import com.io.github.allison.domain.entity.Usuario;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 
 @Service
@@ -13,6 +27,59 @@ public class JwtService {
     @Value("${security.jwt.key-assign}")
     private String keyAssign;
 
+
+    public String generateToken( Usuario usuario){
+        long expString = Long.valueOf(expire);
+        LocalDateTime dateTimeExpire = LocalDateTime.now().plusMinutes(expString);
+        Instant instant = dateTimeExpire.atZone(ZoneId.systemDefault()).toInstant();
+        Date data = Date.from(instant);
+
+
+        return Jwts
+        
+                .builder()
+                .setSubject(usuario.getLogin())
+                .setExpiration(data)
+                .signWith(SignatureAlgorithm.HS512, keyAssign)
+                .compact();
+     
+
+    
+    }
+
+    private Claims getClaims(String token) throws ExpiredJwtException{
+        return Jwts
+                    .parser()
+                    .setSigningKey(keyAssign)
+                    .parseClaimsJws(token)
+                    .getBody();
+    }
+
+    public boolean tokenValid(String token){
+
+        try{
+                Claims claims = getClaims(token);
+                Date datExpire = claims.getExpiration();
+               LocalDateTime localDateTime =  datExpire.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+
+               return !LocalDateTime.now().isAfter(localDateTime);
+
+        }catch(Exception e){
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext contexto = SpringApplication.run(VendasApplication.class);
+        JwtService service = contexto.getBean(JwtService.class);
+
+        Usuario usuario = Usuario.builder().login("allison").build();
+        String token  = service.generateToken(usuario);
+        System.out.println(token);
+
+    }
 
 
 }
