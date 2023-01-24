1826cb7e27a9f1a7c6fd67eab4bc977f0c7a4420
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-24 13:20:48 -0400

    feat: created requestBody authentication user return login

--- vendas/src/main/java/com/io/github/allison/rest/controller/UsuarioController.java
+++ vendas/src/main/java/com/io/github/allison/rest/controller/UsuarioController.java
@@ -3,14 +3,21 @@ package com.io.github.allison.rest.controller;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.io.github.allison.domain.entity.Usuario;
+import com.io.github.allison.exception.PasswordValidException;
+import com.io.github.allison.rest.dto.CredencialsDTO;
+import com.io.github.allison.rest.dto.TokenDTO;
+import com.io.github.allison.security.jwt.JwtService;
 import com.io.github.allison.service.impl.UserServiceImpl;
 
 import lombok.RequiredArgsConstructor;
@@ -22,6 +29,7 @@ public class UsuarioController {
     
     private final UserServiceImpl userService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
@@ -31,6 +39,26 @@ public class UsuarioController {
         return userService.salvar(usuario);
 
     }
+
+    @PostMapping("/auth")
+    public TokenDTO authentication(@RequestBody CredencialsDTO credential){
+        try{
+            Usuario user = Usuario.builder()
+                .login(credential.getLogin())
+                .senha(credential.getSenha())
+                .build();
+
+            UserDetails userAuth = userService.authentication(user);
+            String token = jwtService.generateToken(user);
+            return new TokenDTO(user.getLogin(), token);
+        }catch(UsernameNotFoundException | PasswordValidException e){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+
+      
+        }
+    }
+
+
 
 
 
