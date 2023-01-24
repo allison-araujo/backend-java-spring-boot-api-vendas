2c66f7781ac537a1f9e1f704fe159b88091c35c1
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-24 13:07:11 -0400

    feat: create DTO token and login

---
+++ vendas/src/main/java/com/io/github/allison/rest/dto/CredencialsDTO.java
@@ -0,0 +1,13 @@
+package com.io.github.allison.rest.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CredencialsDTO {
+    
+
+    private String login;
+    private String senha;
+}
