e945b664a44321a63820c37e1d5f9d48c950b8c1
allisonvinicius <allisonvinicius63@gmail.com>
2023-01-29 20:19:44 -0400

    feat: include profiles in maven with build application

--- vendas/src/main/java/com/io/github/allison/VendasApplication.java
+++ vendas/src/main/java/com/io/github/allison/VendasApplication.java
@@ -2,8 +2,9 @@ package com.io.github.allison;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer{
 
    
 
